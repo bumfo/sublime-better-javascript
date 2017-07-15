ec493565dc7456e8f43ce67f32cc0e49f4f58f61
bumfo <bumfo@users.noreply.github.com>
2017-07-14 23:37:39 -0700

    wtf???

--- my_insert_snippet.py
+++ my_insert_snippet.py
@@ -7,21 +7,30 @@ snippets = [
   ['source.js', { 'contents': r'''function ${1}${-1:${1/.+/ /}}(${2}) {
   ${0:$TM_SELECTED_TEXT}
 }''' }],
-  ['meta.object.js', { 'contents': r'''${1:fn} (${2}) {
+  ['meta.object', { 'contents': r'''${1:fn} (${2}) {
   ${0:$TM_SELECTED_TEXT}
 },''' }],
-  ['meta.block.js', { 'contents': r'''function ${1}${-1:${1/.+/ /}}(${2}) {
+  ['meta.block', { 'contents': r'''function ${1}${-1:${1/.+/ /}}(${2}) {
   ${0:$TM_SELECTED_TEXT}
 }''' }],
-  ['meta.class.js', { 'contents': r'''${1:fn} (${2}) {
+  ['meta.class', { 'contents': r'''${1:fn} (${2}) {
   ${0:$TM_SELECTED_TEXT}
 }''' }],
 ]
 
+def single_selector_match(scope_name, selector):
+  selector_list = selector.split('.')
+  for i, scope_component in enumerate(scope_name.split('.')):
+    if (i == len(selector_list)):
+      return True
+    if (selector_list[i] != scope_component):
+      return False
+  return True
+
 def my_score_selector(scope, selector, log):
   for i, scope_name in reversed(list(enumerate(scope.split(' ')))):
     # log("%d" % (scope_name == selector))
-    if scope_name == selector:
+    if single_selector_match(scope_name, selector):
       # log("(%d)" % (i + 1))
       return i + 1
   return 0
