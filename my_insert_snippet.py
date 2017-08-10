aba913beaef5aec891b92fe3dc093783683a09bb
bumfo <bumfo@users.noreply.github.com>
2017-08-10 15:48:14 +0800

    backup

--- my_insert_snippet.py
+++ my_insert_snippet.py
@@ -4,13 +4,13 @@ import sublime_plugin
 from functools import reduce
 
 snippets = [
-  ['source.js', { 'contents': r'''function ${1}${-1:${1/.+/ /}}(${2}) {
+  ['source.js', { 'contents': r'''function ${1}${-1:${1/.+//}}(${2}) {
   ${0:$TM_SELECTED_TEXT}
 }''' }],
   ['meta.object', { 'contents': r'''${1:fn} (${2}) {
   ${0:$TM_SELECTED_TEXT}
 },''' }],
-  ['meta.block', { 'contents': r'''function ${1}${-1:${1/.+/ /}}(${2}) {
+  ['meta.block', { 'contents': r'''function ${1}${-1:${1/.+//}}(${2}) {
   ${0:$TM_SELECTED_TEXT}
 }''' }],
   ['meta.class', { 'contents': r'''${1:fn} (${2}) {
@@ -39,13 +39,13 @@ class MyInsertSnippet(sublime_plugin.TextCommand):
   def run(self, edit):
     i = 0
     for selection in self.view.sel():
-      point = self.view.sel()[i].begin()
+      sel = self.view.sel()[i]
+      point = sel.begin()
 
       mapped = map(lambda snippet: [my_score_selector(self.view.scope_name(point), snippet[0], lambda x: self.view.insert(edit, point, x)), snippet[1]], snippets)
       snippet = reduce(lambda r, x: r if r[0] > x[0] else x, mapped)
 
-      self.view.run_command("left_delete")
-      self.view.run_command("left_delete")
-      self.view.run_command("insert_snippet", snippet[1])
-
       i += 1
+    self.view.run_command("left_delete")
+    self.view.run_command("left_delete")
+    self.view.run_command("insert_snippet", snippet[1])
